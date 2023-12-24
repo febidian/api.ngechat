3a6b1ab8e27943595ce41561592e64fdd6bd806b
febidian <febidiancc321@gmail.com>
2023-12-24 10:49:14 +0700

    merubah table reset-password pada colom token di jadikan unique

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
-            $table->string('token');
+            $table->string('token')->unique();
             $table->timestamp('created_at')->nullable();
         });
     }
